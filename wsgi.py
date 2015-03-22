5e1752fa409eebcfbc3222c187e4c9dac6e20da1
Nick Ficano <nick@keep.com>
2015-03-22 02:12:45 -0400

    OH I get it now :)

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 from werkzeug.wsgi import DispatcherMiddleware
-from flask import Flask
 from nickficano import frontend
 
-application = Flask(__name__)
-application.wsgi_app = DispatcherMiddleware(frontend.create_app())
-application.run(host="0.0.0.0")
+application = DispatcherMiddleware(frontend.create_app())
