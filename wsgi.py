5db389aee053c5b640b880fd3721b035dd8a0888
Nick Ficano <nick@keep.com>
2015-03-22 00:19:31 -0400

    let's see if this works :/

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,6 @@ from werkzeug.wsgi import DispatcherMiddleware
 from nickficano import frontend
 
 app = DispatcherMiddleware(frontend.create_app())
+
+if __name__ == '__main__':
+    app.app.run(host='0.0.0.0')
