2a4d7699dcf2ba9253129f45236434f89540cb7a
Nick Ficano <nick@keep.com>
2015-03-22 01:19:55 -0400

    renamed app to application

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from werkzeug.wsgi import DispatcherMiddleware
 from flask import Flask
 from nickficano import frontend
 
-app = Flask(__name__)
-app.wsgi_app = DispatcherMiddleware(frontend.create_app())
-app.run(host="0.0.0.0")
+application = Flask(__name__)
+application.wsgi_app = DispatcherMiddleware(frontend.create_app())
+application.run(host="0.0.0.0")
