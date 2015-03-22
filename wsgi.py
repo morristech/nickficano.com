c0ef536afeba81c5cee2f8fdf5c4f01310f4176d
Nick Ficano <nick@keep.com>
2015-03-22 01:17:16 -0400

    new attempt at wiring up DispatcherMiddleware

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from werkzeug.wsgi import DispatcherMiddleware
-
+from flask import Flask
 from nickficano import frontend
 
-app = DispatcherMiddleware(frontend.create_app())
-
-if __name__ == '__main__':
-    app.app.run(host='0.0.0.0')
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(frontend.create_app())
+app.run(host="0.0.0.0")
