b2f162b0e402ec5bce2c225778f5bb44a04c6684
Nick Ficano <nick@keep.com>
2015-03-31 23:26:48 -0400

    added fabfile to handle admin tasks (e.g.: deploy)

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from fabric.api import cd
+from fabric.api import env
+from fabric.api import sudo
+
+env.hosts = [
+  'nickficano.com',
+]
+env.use_ssh_config = True
+
+
+def deploy():
+    git_pull()
+    restart_uwsgi()
+
+
+def git_pull():
+  with cd('~/nickficano.com/app'):
+      sudo('git pull origin master', user='www-data')
+
+
+def restart_uwsgi():
+  sudo("supervisorctl restart www_uwsgi")
