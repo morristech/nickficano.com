1dd54427009830ab4bcadd104a45757e3c76b864
Nick Ficano <nick@keep.com>
2015-04-02 18:21:16 -0400

    some cryptic fun
    lets show the pid on the landing page

--- nickficano/frontend/general.py
+++ nickficano/frontend/general.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Blueprint
 from flask import render_template as T
 from . import route
@@ -8,4 +9,5 @@ bp = Blueprint('index', __name__)
 
 @route(bp, '/')
 def index():
-    return T('index.j2')
+    pid = os.getpid()
+    return T('index.j2', pid=pid)
