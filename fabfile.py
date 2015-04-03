c5c87c1c67d57da5c007a4edd7a7a51d6325ee5e
Nick Ficano <nick@keep.com>
2015-04-02 22:09:06 -0400

    wrote method to update dependencies

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+from contextlib import contextmanager
 from fabric.api import cd
 from fabric.api import env
+from fabric.api import prefix
+from fabric.api import run
 from fabric.api import sudo
 
 env.hosts = ['nickficano.com']
 # Use authentication information stored in `~/.ssh/config`.
 env.use_ssh_config = True
+
+
+@contextmanager
+def virtualenv(name):
+    with prefix("workon {}".format(name)):
+        yield
 
 
 def deploy():
@@ -26,3 +35,11 @@ def restart_uwsgi():
   """Gracefully restart uwsgi.
   """
   sudo("supervisorctl restart www_uwsgi")
+
+
+def update_dependencies():
+    """Ensure all pip dependencies are up-to-date.
+    """
+    with virtualenv("nickficano.com"):
+        with cd('~/nickficano.com/app'):
+            run("pip install --ignore-installed -r requirements.txt")
