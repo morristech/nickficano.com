ecfe00b42d6680c6d791bd0f7ed1d27c7d29d1c3
Nick Ficano <nick@keep.com>
2015-04-27 21:54:55 -0400

    versions string, no cache

--- nickficano/frontend/assets.py
+++ nickficano/frontend/assets.py
@@ -6,12 +6,11 @@ from flask.ext.assets import Environment
 css_all = Bundle("scss/all.scss",
                  depends=["**/*.scss"],
                  filters="pyscss,cssmin",
-                 output="css/all.css",
-                 debug=True)
+                 output="css/%(version)s/all.css")
 
 
 def init_app(app):
     webassets = Environment(app)
     webassets.register('css_all', css_all)
-    webassets.cache = not app.debug
     webassets.debug = app.debug
+    webassets.auto_rebuild = app.debug
