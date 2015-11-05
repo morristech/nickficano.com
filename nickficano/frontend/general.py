6f6c04c20f0df50d472f25c4b27daa49dbb226f8
Nick Ficano <nficano@gmail.com>
2015-11-05 02:36:28 -0500

    this was a dumb idea

--- nickficano/frontend/general.py
+++ nickficano/frontend/general.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-from contextlib import contextmanager
 
 from flask import Blueprint
 from flask import render_template as T
-from fabric.api import cd, run, sudo, prefix
 from . import route
 
 bp = Blueprint('index', __name__)
@@ -14,25 +12,3 @@ bp = Blueprint('index', __name__)
 def index():
     pid = os.getpid()
     return T('index.j2', pid=pid)
-
-
-@contextmanager
-def virtualenv(name):
-    """Handy context manager to activate a virtualenv.
-    """
-    with prefix('WORKON_HOME=$HOME/.virtualenvs'):
-        with prefix('source /usr/local/bin/virtualenvwrapper.sh'):
-            with prefix("workon {}".format(name)):
-                yield
-
-
-@route(bp, '/gendo', methods=('GET', 'POST',))
-def gendo_webhook():
-    with cd('~/gendo/'):
-        sudo('git pull origin master', user='ubuntu')
-
-    with virtualenv("gendo"):
-        with cd('~/gendo/'):
-            run("pip install --ignore-installed -r requirements.txt")
-    sudo("supervisorctl restart gendobot")
-    return "OK"
