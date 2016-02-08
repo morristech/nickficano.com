546b2f5e3f4dd800d6d9799167760fcdc4d97a3a
Nick Ficano <nficano@gmail.com>
2016-02-08 17:26:37 -0500

    switched to click

---
+++ manage.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import click
+
+from werkzeug.serving import run_simple
+from wsgi import application
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--port', default=5000, help='The port for the server.')
+@click.option('--hostname', default='localhost', help=(
+    "The host for the application. eg: 'localhost'"))
+def runserver(hostname, port):
+    run_simple(hostname, port, application,
+               use_reloader=True, use_debugger=True)
+
+if __name__ == '__main__':
+    cli.add_command(runserver)
+    cli()
