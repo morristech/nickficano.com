e98d6301b46f12f1a7c25c045501ff75687c6c94
Nick Ficano <nficano@gmail.com>
2016-02-08 17:29:39 -0500

    application -> app

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import click
 
 from werkzeug.serving import run_simple
-from wsgi import application
+from wsgi import application as app
 
 
 @click.group()
@@ -16,8 +16,7 @@ def cli():
 @click.option('--hostname', default='localhost', help=(
     "The host for the application. eg: 'localhost'"))
 def runserver(hostname, port):
-    run_simple(hostname, port, application,
-               use_reloader=True, use_debugger=True)
+    run_simple(hostname, port, app, use_reloader=True, use_debugger=True)
 
 if __name__ == '__main__':
     cli.add_command(runserver)
