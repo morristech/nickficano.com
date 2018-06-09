651154342a1b4df41195cab3dcccae90987b3663
Nick Ficano <nficano@gmail.com>
2018-06-09 14:38:18 -0400

    find-my-iphone api

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 from werkzeug.wsgi import DispatcherMiddleware
 
 from server.apps import frontend
+from server.apps import api
 
 # This is the entry point for callable or entry point function for uwsgi.
 application = DispatcherMiddleware(
-    frontend.create_app(), {},
+    frontend.create_app(), {
+        '/api': api.create_app()
+    },
 )
