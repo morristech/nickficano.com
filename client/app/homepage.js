1bc3d6bfbc5ce4e8d586dfec6290547f39a38b45
Nick Ficano <nficano@gmail.com>
2018-08-28 11:54:33 -0400

    better particles

--- client/app/homepage.js
+++ client/app/homepage.js
@@ -18,10 +18,10 @@ function initHeader() {
 
     // create points
     points = [];
-    for(var x = 0; x < width; x = x + width/20) {
-        for(var y = 0; y < height; y = y + height/20) {
-            var px = x + Math.random()*width/20;
-            var py = y + Math.random()*height/20;
+    for(var x = 0; x < width; x = x + width/15) {
+        for(var y = 0; y < height; y = y + height/15) {
+            var px = x + Math.random()*width/15;
+            var py = y + Math.random()*height/15;
             var p = {x: px, originX: px, y: py, originY: py };
             points.push(p);
         }
@@ -66,27 +66,9 @@ function initHeader() {
 
 // Event handling
 function addListeners() {
-    if(!('ontouchstart' in window)) {
-        window.addEventListener('mousemove', mouseMove);
-    }
     window.addEventListener('scroll', scrollCheck);
     window.addEventListener('resize', resize);
 }
-
-function mouseMove(e) {
-    var posx = posy = 0;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY)    {
-        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-    target.x = posx;
-    target.y = posy;
-}
-
 function scrollCheck() {
     if(document.body.scrollTop > height) animateHeader = false;
     else animateHeader = true;
