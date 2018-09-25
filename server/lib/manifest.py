3b09b0851351a4194254a64e7453970ef1ee7072
Nick Ficano <nficano@gmail.com>
2018-09-25 06:28:37 -0400

    fixed manifest, added logging

--- server/lib/manifest.py
+++ server/lib/manifest.py
@@ -1,8 +1,9 @@
 import os
 import json
+import server
 
 def get_webpack_manifest():
-    fp = os.path.join(os.getcwd(), 'manifest.json')
+    fp = os.path.join(os.path.dirname(server.__file__), '..', 'manifest.json')
     with open(fp) as fh:
         manifest = json.loads(fh.read())
         return {k: os.path.basename(v) for k, v in manifest.items()}
